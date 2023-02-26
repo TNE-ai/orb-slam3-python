a1ecd57ee873cc20fc92ead7adc1ffbfad45b762
Rakuto Furutani <rakuto@gmail.com>
2023-02-25 20:34:08 -0800

    initial commit

---
+++ orb_slam3/__init__.py
@@ -0,0 +1,15 @@
+from _orb_slam3 import *
+
+
+class Sensor:
+    MONOCULAR = 0
+    STEREO = 1
+    RGBD = 2
+    IMU_MONOCULAR = 3
+    IMU_STEREO = 4
+    IMU_RGBD = 5
+
+
+class FileType:
+    TEXT = 0
+    BINARY = 1
