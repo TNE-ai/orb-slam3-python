cde74c5a81b4a626d877a9ad8e5d8751c1037038
Rakuto Furutani <rakuto@gmail.com>
2023-02-27 23:13:46 -0800

    chore: clean up build scripts

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import subprocess
+import shutil
 from pathlib import Path
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
@@ -29,16 +30,28 @@ class CMakeBuild(build_ext):
             f"-DCMAKE_BUILD_TYPE={cfg}",
         ]
 
-        build_args = [
-            f"-j{os.cpu_count()}"
-        ]
+        build_args = []
+
+        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
+            # self.parallel is a Python 3 only way to set parallel jobs by hand
+            # using -j in the build_ext call, not supported by pip or PyPA-build.
+            if hasattr(self, "parallel") and self.parallel:
+                # CMake 3.12+ only.
+                build_args += [f"-j{self.parallel}"]
 
         if "CMAKE_ARGS" in os.environ:
             cmake_args += [x for x in os.environ["CMAKE_ARGS"].split(" ") if x]
 
         build_temp = Path(self.build_temp) / ext.name
         if not build_temp.exists():
-            build_temp.mkdir(parents=True)
+          build_temp.mkdir(parents=True)
+
+        # Build ORB-SLAM3 and dependencies
+        subprocess.run(
+            ["./build_libs.sh"], cwd=os.path.join(build_temp, "vendor", "ORB_SLAM3")
+        )
+        # FIXME(rakuto): Workaround for broken linker...
+        shutil.copyfile(Path.cwd() / "vendor" / "ORB_SLAM3" / "lib" / "libORB_SLAM3.so", "/usr/local/lib/libORB_SLAM3.so")
 
         subprocess.run(
             ["cmake", ext.source_dir, *cmake_args], cwd=build_temp, check=True
@@ -59,6 +72,5 @@ setup(
     zip_safe=False,
     extras_require={"test": ["pytest>=6.0"]},
     python_requires=">=3.8",
-    packages=[''],
-    package_data={'': ['ORB_SLAM3/lib/libORB_SLAM3.so']}
+    packages=["orb_slam3"],
 )
